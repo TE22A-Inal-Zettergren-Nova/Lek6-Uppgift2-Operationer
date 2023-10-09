eb56e04c34d34a96bb39aadcdffda141e045e9d4
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2023-10-09 09:34:52 +0200

    gjort en scanner

---
+++ src/App.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        /*Uppgift a */
+
+        Scanner tb = new Scanner(System.in);
+        int talet = tb.nextInt();
+    }
+}
