ebc031a044527cda546253c3733ec5338c249e22
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2023-10-11 10:53:34 +0200

    gör om bråktal till blandad form genom modulo

--- src/App.java
+++ src/App.java
@@ -4,12 +4,26 @@ public class App {
     public static void main(String[] args) throws Exception {
         /*Uppgift a */
         Scanner tb = new Scanner(System.in);
-        System.out.println("Skriv in ett tusental: ");
+        System.out.print("Skriv in ett tusental: ");
         int talet = tb.nextInt();
         talet /= 1000;
         Math.round(talet);
         System.out.println(talet*1000);
         
+        /*Uppgift b */
+        System.out.println("Skriv in täljare : ");
+        int tälj = tb.nextInt();
+        System.out.println("Skriv in en nämnare: ");
+        int nämn = tb.nextInt();
+
+        System.out.println((tälj/nämn)+" "+(tälj%nämn)+"/"+nämn);
+
+         
+
+
+
+
+
     }
 }
 
