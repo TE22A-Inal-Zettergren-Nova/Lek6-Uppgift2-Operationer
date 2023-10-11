c827cf7181a6e6281ad3041f78d5f7c0aa3d9b2d
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2023-10-11 10:08:23 +0200

    skapat program som  kan avrunda tusental

--- src/App.java
+++ src/App.java
@@ -3,8 +3,13 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         /*Uppgift a */
-
         Scanner tb = new Scanner(System.in);
+        System.out.println("Skriv in ett tusental: ");
         int talet = tb.nextInt();
+        talet /= 1000;
+        Math.round(talet);
+        System.out.println(talet*1000);
+        
     }
 }
+
